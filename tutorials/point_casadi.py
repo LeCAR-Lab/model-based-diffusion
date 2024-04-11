6a16277ba87fb7bfdaabaa3431fbae08cff89b14
jc-bao <jc-bao@outlook.com>
2024-04-11 09:35:09 -0400

    :zap: add trajopt example with casadi
    Former-commit-id: d18521c4fc0c1b1959394affb69d0042810f89be

--- tutorials/point_casadi.py
+++ tutorials/point_casadi.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 opti = ca.Opti()
 
 # Define the time horizon and number of control intervals
-T = 1  # Time horizon
+T = 3.0  # Time horizon
 N = 10  # Number of control intervals
 dt = T/N  # Time step
 
@@ -14,34 +14,36 @@ dt = T/N  # Time step
 x0 = np.array([-1.0, 0])
 xf = np.array([1.0, 0])
 
+# Define observation terms
+
 # Define the states and controls
-x = opti.variable(2, N+1)  # Position (x, y)
-u = opti.variable(2, N)  # Velocity (vx, vy)
-u_max = 3.0  # Maximum velocity
+x = opti.variable(N+1, 2)  # Position (x, y)
+u = opti.variable(N, 2)  # Velocity (vx, vy)
+u_max = 1.0  # Maximum velocity
 
 # Define the objective function
 objective = 0
 Q = np.diag([0.5, 0.5])  # Weight matrix for the states
 R = np.diag([0.1, 0.1])  # Weight matrix for the controls
 for k in range(N):
-    objective += (ca.mtimes([x[:, k].T, Q, x[:, k]]) + ca.mtimes([u[:, k].T, R, u[:, k]]))
+    objective += (ca.mtimes([x[k].T, Q, x[k]]) + ca.mtimes([u[k].T, R, u[k]]))
 
 opti.minimize(objective)
 
 # Define the dynamic constraints
 for k in range(N):
-    opti.subject_to(x[:, k+1] == x[:, k] + dt * u[:, k])  # Dynamics constraint
-    opti.subject_to(u[:, k] <= u_max)  # Maximum velocity constraint
+    opti.subject_to(x[k+1] == x[k] + dt * u[k])  # Dynamics constraint
+    opti.subject_to(u[k] <= u_max)  # Maximum velocity constraint
 
 # Define the initial and final boundary conditions
-opti.subject_to(x[:, 0] == x0)
-opti.subject_to(x[:, -1] == xf)
+opti.subject_to(x[0] == x0)
+opti.subject_to(x[-1] == xf)
 
 # Define the obstacle avoidance constraint
 obs_center = np.array([0, 0])  # Center of the obstacle
 obs_radius = 0.5  # Radius of the obstacle
 for k in range(N+1):
-    opti.subject_to(ca.dot(x[:, k] - obs_center, x[:, k] - obs_center) >= obs_radius**2)
+    opti.subject_to(ca.dot(x[k] - obs_center, x[k] - obs_center) >= obs_radius**2)
 
 # Set initial guess
 opti.set_initial(x, np.linspace(x0, xf, N+1).T)
