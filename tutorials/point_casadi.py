a0cb0f818833e065fe3a1a3b76c72d925f4f7358
jc-bao <jc-bao@outlook.com>
2024-04-11 10:14:21 -0400

    :zap: add trajopt example with casadi
    Former-commit-id: e08a0e325537111aa41f782dcfe93090bbc9c35c

--- tutorials/point_casadi.py
+++ tutorials/point_casadi.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define the time horizon and number of control intervals
-T = 2.0  # Time horizon
-dt = 0.2  # Time step
+T = 3.0  # Time horizon
+dt = 0.1  # Time step
 N = int(T//dt)  # Number of control intervals
 Q = np.diag([0.5, 0.5])  # Weight matrix for the states
 R = np.diag([0.1, 0.1])  # Weight matrix for the controls
@@ -28,14 +28,15 @@ def optimize_trajectory(x_guess, u_guess, yxs, yus, noise_var):
         # cost function
         objective += ca.mtimes([(x[:, k] - xf).T, Q, x[:, k] - xf]) + ca.mtimes([u[:, k].T, R, u[:, k]])
         # observation terms
-        objective += ca.mtimes([(yxs[:, k] - x[:, k]).T, np.eye(2), yxs[:, k] - x[:, k]]) / noise_var
-        objective += ca.mtimes([(yus[:, k] - u[:, k]).T, np.eye(2), yus[:, k] - u[:, k]]) / noise_var
+        # objective += ca.mtimes([(yxs[:, k] - x[:, k]).T, np.eye(2), yxs[:, k] - x[:, k]]) / noise_var
+        # objective += ca.mtimes([(yus[:, k] - u[:, k]).T, np.eye(2), yus[:, k] - u[:, k]]) / noise_var
     opti.minimize(objective)
     
     # Define the dynamic constraints
     for k in range(N):
         opti.subject_to(x[:, k+1] == x[:, k] + dt * u[:, k])  # Dynamics constraint
         opti.subject_to(u[:, k] <= u_max)  # Maximum velocity constraint
+        opti.subject_to(u[:, k] >= -u_max)  # Minimum velocity constraint
     
     # Define the initial and final boundary conditions
     opti.subject_to(x[:, 0] == x0)
@@ -43,8 +44,9 @@ def optimize_trajectory(x_guess, u_guess, yxs, yus, noise_var):
     
     # Define the obstacle avoidance constraint
 
-    for k in range(N+1):
-        opti.subject_to(ca.dot(x[:, k] - obs_center, x[:, k] - obs_center) >= obs_radius**2)
+    # for k in range(N+1):
+    #     for i in range(obs_center.shape[0]):
+    #         opti.subject_to(ca.dot(x[:, k] - obs_center[i], x[:, k] - obs_center[i]) >= obs_radius**2)
     
     # Set initial guess
     opti.set_initial(x, x_guess)
@@ -69,16 +71,19 @@ def optimize_trajectory(x_guess, u_guess, yxs, yus, noise_var):
     return x_opt, u_opt, yxs_new, yus_new
 
 def plot_traj(ax, x, u, ys, yu):
+    ax.clear()
     # obstacles
     for i in range(obs_center.shape[0]):
         circle = plt.Circle(obs_center[i, :], obs_radius, color='r', fill=False)
         ax.add_artist(circle)
-    ax.plot(x[0, :], x[1, :], 'b-', label='Optimized Trajectory')
-    ax.plot(ys[0, :], ys[1, :], 'ro', label='Observations')
-    ax.quiver(x[0, :], x[1, :], u[0, :], u[1, :], color='b')
-    ax.quiver(ys[0, :], ys[1, :], yu[0, :], yu[1, :], color='r')
+    ax.plot(x[0, :], x[1, :], 'b-o', label='Optimized Trajectory')
+    # ax.quiver(x[0, 1:], x[1, 1:], u[0, :], u[1, :], color='b')
+    # ax.plot(ys[0, :], ys[1, :], 'ro', label='Observations')
+    # ax.quiver(ys[0, 1:], ys[1, 1:], yu[0, :], yu[1, :], color='r')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
     ax.legend()
     ax.set_aspect('equal')
     ax.grid(True)
@@ -97,7 +102,8 @@ yus = np.random.normal(0, np.sqrt(noise_vars[0]), (2, N))
 fig, ax = plt.subplots()
 for (i, noise_var) in enumerate(noise_vars):
     x_opt, u_opt, yxs, yus = optimize_trajectory(x_guess, u_guess, yxs, yus, noise_var)
-    
+    print(u_opt)
     # Plot the optimized trajectory
     plot_traj(ax, x_opt, u_opt, yxs, yus)
     plt.pause(0.01)
+plt.show()
