37a2aeafb8ef018402461e282ec3d7ba7626ea12
jc-bao <jc-bao@outlook.com>
2024-05-09 20:30:46 -0400

    Update PPO training parameters for the "walker2d" environment
    Former-commit-id: 592e38428eacd9a43812679c6e57036ce495265a

--- mbd/scripts/train_brax.py
+++ mbd/scripts/train_brax.py
@@ -66,22 +66,22 @@ train_fn = {
         seed=1,
     ),
     "walker2d": functools.partial(
-        sac.train,
-        num_timesteps=7_864_320,
+        ppo.train,
+        num_timesteps=50_000_000,
         num_evals=20,
-        reward_scaling=1.0,
-        episode_length=100,
+        reward_scaling=1,
+        episode_length=1000,
         normalize_observations=True,
         action_repeat=1,
-        discounting=0.997,
-        learning_rate=6e-4,
-        num_envs=128,
-        batch_size=128,
-        grad_updates_per_step=32,
-        max_devices_per_host=1,
-        max_replay_size=1048576,
-        min_replay_size=8192,
-        seed=1,
+        unroll_length=20,
+        num_minibatches=32,
+        num_updates_per_batch=8,
+        discounting=0.95,
+        learning_rate=3e-4,
+        entropy_cost=0.001,
+        num_envs=2048,
+        batch_size=512,
+        seed=3,
     ),
     "halfcheetah": functools.partial(
         ppo.train,
@@ -183,7 +183,7 @@ def progress(num_steps, metrics):
     times.append(datetime.now())
     xdata.append(num_steps)
     ydata.append(metrics["eval/episode_reward"])
-    print(f"step: {num_steps}, reward: {metrics['eval/episode_reward']:.2f}")
+    print(f"step: {num_steps}, episode return: {metrics['eval/episode_reward']:.2f}")
 
 
 make_inference_fn, params, _ = train_fn(environment=env, progress_fn=progress)
